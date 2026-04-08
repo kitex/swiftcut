95fdbfc9b11b06edeeacfc0e0eb47a62e60af5c8
sugandha <sugandha.amatya@gmail.com>
2026-04-08 06:27:34 -0500

    Initial Commit

---
+++ SwiftCut/SwiftCutApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftCutApp.swift
+//  SwiftCut
+//
+//  Created by Sugandha Amatya on 4/8/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftCutApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SwiftCutTests/SwiftCutTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftCutTests.swift
+//  SwiftCutTests
+//
+//  Created by Sugandha Amatya on 4/8/26.
+//
+
+import Testing
+@testable import SwiftCut
+
+struct SwiftCutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
